aee450285dd1a9ebd6d6e35239a428b79a3e06c7
SevenLawless <7lawless25@gmail.com>
2026-01-28 11:40:38 +0100

    2nd done

--- week_1/day_1/exercises/exercises_xp_gold.js
+++ week_1/day_1/exercises/exercises_xp_gold.js
@@ -1,9 +1,44 @@
 // ===== Exercise 1
 
+let numbers = [123, 8409, 100053, 333333333, 7]
+ for (let number of numbers) {
+    if (number % 3 === 0) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
 // ===== Exercise 2
+
+let guestList = {
+  randy: "Germany",
+  karla: "France",
+  wendy: "Japan",
+  norman: "England",
+  sam: "Argentina"
+}
+
+let stuname = prompt("Please enter your name:").toLowerCase();
+
+
+if (stuname in guestList) {
+    console.log(`Hi! I'm ${stuname}, and I'm from ${guestList[stuname]}.`);
+} else {
+    console.log("Hi! I'm a guest.");
+}
 
 // ===== Exercise 3
 
-// ===== Exercise 4
+let age = [20,5,12,43,98,55];
+let sum = 0;
+let highest = age[0];
 
-// ===== Exercise ...
+for (let i=0; i<age.length; i++) {
+    sum += age[i];
+    if (age[i] > highest) {
+        highest = age[i];
+    }
+}
+console.log(`The sum is: ${sum}`);
+console.log(`The highest is: ${highest}`);
