099017b6c836ef80c81f224929c5119575089ae3
SevenLawless <7lawless25@gmail.com>
2026-01-28 12:37:03 +0100

    day 1 done

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,39 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge Not Bad
 
-// ====== Daily Challenge 2
+let sentence = "not that bad brojola";
+//let sentence = "very bad, not very good";
 
-// ====== Daily Challenge 3
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+
+if (wordBad > wordNot && wordBad !== -1 && wordNot !== -1) {
+    let newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+    console.log(`the result is : '${newSentence}'`);
+} else {
+    console.log(`normal sentence is : '${sentence}'`);
+}
+
+
+
+
+// ====== Daily Challenge Stars
+
+let stars = "";
+for (let i = 0; i < 6; i++) {
+    stars += "* ";
+    console.log(`${stars}\n`);
+}
+
+
+
+for (let i = 0; i < 6; i++) {
+    let line = "";
+    for (let j = 0; j <= i; j++) {
+        line += "* ";
+    }
+    console.log(line);
+}
+
+
+// ofc imma do all the challenges by myself hh
