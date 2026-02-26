ed4c26a787182011c20d2c05bea130a2b95d6ba8
SevenLawless <7lawless25@gmail.com>
2026-02-26 14:43:18 +0000

    jil

--- week_3/day_1/daily_challenge/challenges.js
+++ week_3/day_1/daily_challenge/challenges.js
@@ -1,12 +1,43 @@
 // 1st daily challenge 
 
 
+const makeAllCaps = (arr) => {
+    return new Promise((resolve, reject) => {
+        const allStrings = arr.every(word => typeof word === "string")
+
+        if(allStrings) {
+            resolve(arr.map(word => word.toUpperCase()))
+        } else {
+            reject("array is not all strings.")
+        }
+    })
+}
+
+const sortWords = (arr) => {
+    return new Promise((resolve, reject) => {
+        if(arr.length > 4) {
+            resolve(arr.sort())
+        } else {
+            reject("array is not bigger than 4")
+        }
+    })
+}
 
 
+makeAllCaps([1, "pear", "banana"])
+    .then(arr => sortWords(arr))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
+makeAllCaps(["apple", "pear", "banana"])
+    .then(arr => sortWords(arr))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
-
-
+makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
+    .then(arr => sortWords(arr))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
 
 
